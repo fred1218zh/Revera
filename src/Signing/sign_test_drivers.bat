dc97f970f8db851fa996d590f8593c5f8ca58320
Fred Zhang <fred1218zh@outlook.com>
2017-07-19 20:28:28 +0800

    Added signing bat file only for drivers

---
+++ src/Signing/sign_test_drivers.bat
@@ -0,0 +1,19 @@
+PATH=%PATH%;%WSDK81%\bin\x86;
+
+set PFXNAME=TestCertificate\idrix_codeSign.pfx
+set PFXPASSWORD=idrix
+set PFXCA=TestCertificate\idrix_TestRootCA.crt
+set SHA256PFXNAME=TestCertificate\idrix_Sha256CodeSign.pfx
+set SHA256PFXPASSWORD=idrix
+set SHA256PFXCA=TestCertificate\idrix_SHA256TestRootCA.crt
+
+set SIGNINGPATH=%~dp0
+cd %SIGNINGPATH%
+
+rem sign using SHA-1
+signtool sign /v /a /f %PFXNAME% /p %PFXPASSWORD% /ac %PFXCA% /fd sha1 /t http://timestamp.verisign.com/scripts/timestamp.dll "..\Release\Setup Files\veracrypt.sys" "..\Release\Setup Files\veracrypt-x64.sys"
+
+rem sign using SHA-256
+signtool sign /v /a /f %SHA256PFXNAME% /p %SHA256PFXPASSWORD% /ac %SHA256PFXCA% /as /fd sha256 /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 "..\Release\Setup Files\veracrypt.sys" "..\Release\Setup Files\veracrypt-x64.sys"
+
+pause
